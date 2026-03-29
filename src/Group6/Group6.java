0366b1a38d88d36ed74e3668422d09222efda77e
Hoang Quoc Huy <Hoang Quoc Huy@172.22.195.32>
2026-03-29 13:18:51 +0700

    Lưu trên máy

--- src/Group6/Group6.java
+++ src/Group6/Group6.java
@@ -12,7 +12,8 @@ public class Group6 {
                 break;
             }
         }
-
+        
         System.out.println(isPrime ? "La so nguyen to" : "Khong phai");
+        System.out.println("Không có lỗi!");
     }
 }
